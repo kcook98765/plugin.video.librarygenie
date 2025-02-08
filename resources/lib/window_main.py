4e9a7e33d1da7850c8f3f509ec9ad3eae279732a
kcook98765 <56806582+kcook98765@users.noreply.github.com>
2025-02-08 07:47:00 +0000

    Assistant checkpoint: Update list control and folder indentation
    Assistant generated file changes:
    - resources/lib/window_main.py: Update list control to allow icon indentation, Update folder display with proper indentation
    
    ---
    
    User prompt:
    
    that did not appear to work, all folders , no matter depth, still show icon on the far left with no indentation

--- resources/lib/window_main.py
+++ resources/lib/window_main.py
@@ -52,7 +52,7 @@ class MainWindow(BaseWindow):
 
         # File browser list
         self.list_control = pyxbmct.List(_imageWidth=25, _imageHeight=25,
-                                         _itemTextXOffset=0, _itemHeight=30, _space=2)
+                                         _itemTextXOffset=25, _itemHeight=30, _space=2)
         self.placeControl(self.list_control, 2, 0, rowspan=9, columnspan=10, pad_x=5, pad_y=5)
         self.connect(self.list_control, self.on_list_item_click)
 
@@ -372,9 +372,8 @@ class MainWindow(BaseWindow):
             folder_label = f"{indent_str}[B][COLOR {color}]{folder['name']} ({folder_media_count})[/COLOR][/B]"
         else:
             folder_label = f"{indent_str}{folder['name']} ({folder_media_count})"
-        folder_item = xbmcgui.ListItem(folder_label)
-        folder_item.setArt({'icon': self.folder_icon, 'thumb': self.folder_icon})
-        folder_item.setProperty('indent', indent_str)
+        folder_item = xbmcgui.ListItem(' ' * (indent * self.INDENTATION_MULTIPLIER) + folder_label)
+        folder_item.setArt({'icon': ' ' * (indent * self.INDENTATION_MULTIPLIER) + self.folder_icon, 'thumb': self.folder_icon})
         folder_item.setProperty('isFolder', 'true')
         folder_item.setProperty('folder_id', str(folder['id']))
         folder_item.setProperty('expanded', str(expanded))
