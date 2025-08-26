65ae22b75dffcf6acb166639d3c7679d20606706
kcook98765 <56806582+kcook98765@users.noreply.github.com>
2025-08-26 00:11:34 +0000

    start fresh

--- resources/lib/config/addon_ref.py
+++
@@ -1,9 +0,0 @@
-import xbmcaddon
-from functools import lru_cache
-
-ADDON_ID = "plugin.video.librarygenie"
-
-@lru_cache(maxsize=1)
-def get_addon():
-    # Always pass an explicit id so Kodi doesn't need to infer context
-    return xbmcaddon.Addon(id=ADDON_ID)
